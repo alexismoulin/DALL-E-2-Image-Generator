65720ec50e401367505d7b8bae62dc51c40baf3a
Alexis Moulin <alexis.moulin@icloud.com>
2023-01-30 17:50:43 +0100

    Backbone of the app

--- DALL-E-2 Generator/ContentView.swift
+++ DALL-E-2 Generator/ContentView.swift
@@ -1,12 +1,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var prompt: String = ""
+    @State private var responseURL: URL?
+    @State private var displayImage: Bool = false
+    
     var body: some View {
+        
+        // Textfield
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Test", text: $prompt)
+                .textFieldStyle(.roundedBorder)
+            Button("Post") {
+                Task {
+                    do {
+                        displayImage = true
+                        responseURL = await URL(string: try NetworkRequest.postRequest(prompt: prompt))
+                    } catch {
+                        fatalError(error.localizedDescription)
+                    }
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            
+            // Image
+            if displayImage {
+                AsyncImage(url: responseURL) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image.resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: 300, maxHeight: 300)
+                    case .failure:
+                        Image(systemName: "photo")
+                    @unknown default:
+                        EmptyView()
+                    }
+                }
+            }
         }
         .padding()
     }
---
+++ DALL-E-2 Generator/Networking/NetworkRequest.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+class NetworkRequest {
+    
+    // Encode sent data
+    struct MyJasonObject: Codable {
+        let prompt: String
+    }
+    
+    // Decode received data
+    struct ReceivedData: Codable {
+        let created: Int
+        let data: [RawImageURL]
+    }
+    
+    struct RawImageURL: Codable {
+        let url: String
+    }
+
+    static func postRequest(prompt: String) async throws -> String {
+        // Prepare prompt for sending the post request
+        let myJasonObject = MyJasonObject(prompt: prompt)
+        let payload = try JSONEncoder().encode(myJasonObject)
+        // Prepare the URL request
+        guard let url = URL(string:"https://api.openai.com/v1/images/generations") else { fatalError("Cannot make the URL request")}
+        var urlRequest = URLRequest(url: url)
+        let appKey = Bundle.main.object(forInfoDictionaryKey: "IMAGE_API_KEY") as? String
+        urlRequest.addValue("Bearer \(appKey!)", forHTTPHeaderField: "Authorization")
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpMethod = "POST"
+        // Wait and process Server feedback
+        let (data, _) = try await URLSession.shared.upload(for: urlRequest, from: payload)
+        let successInfo = try JSONDecoder().decode(ReceivedData.self, from: data)
+        let imageURL = successInfo.data[0].url
+        // return the image URL
+        return imageURL
+    }
+}
