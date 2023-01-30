c835efec8040cd8567d835d2cd36aed88ba21a6e
Alexis Moulin <alexis.moulin@icloud.com>
2023-01-30 16:45:21 +0100

    Initial Commit

---
+++ DALL-E-2 Generator/DALL_E_2_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DALL_E_2_GeneratorApp.swift
+//  DALL-E-2 Generator
+//
+//  Created by Alexis Moulin on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct DALL_E_2_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
